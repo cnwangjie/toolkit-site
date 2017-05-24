2a9b705b821eafba1b1b907ef470c4c545845418
Wang Jie <doudou19758@gmail.com>
2017-05-24 21:51:53 +0800

    fix tool (qrcode-scan)

--- tools/qrcode-scan/static/script.js
+++ tools/qrcode-scan/static/script.js
@@ -4,7 +4,20 @@ var result = document.getElementById('result')
 input.onchange = function() {
     var r = new FileReader()
     r.onload = function() {
-        result.innerText = qrcode.decode(r.result)
+        var cvs = document.createElement('canvas')
+        cvs.id = 'qr-canvas'
+        cvs.style.display = 'none'
+        document.body.append(cvs)
+        var ctx = cvs.getContext('2d')
+        var img = new Image
+        img.src = r.result
+        img.onload = function() {
+            cvs.width = img.width
+            cvs.height = img.height
+            ctx.drawImage(img, 0, 0, img.width, img.height)
+            var re = qrcode.decode()
+            result.innerText = re
+        }
     }
     r.readAsDataURL(input.files[0])
 }
